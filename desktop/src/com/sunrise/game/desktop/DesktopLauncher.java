1e3de7ec7ef1523fef68f3b323ef3e508898074d
Ibrahim Saberi <georgeipsum@gmail.com>
2017-06-15 03:33:51 -0400

    Pushing initial directory

---
+++ desktop/src/com/sunrise/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sunrise.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sunrise.game.SUNRISE;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SUNRISE(), config);
+	}
+}
